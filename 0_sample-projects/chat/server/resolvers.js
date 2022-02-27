156fac48c6deec8175643d463cd60a1e7840ac9b
johannesstroebele91 <stroebele.j@gmail.com>
2022-02-27 19:23:44 +0100

    add subscription resolver using pubsub

--- 0_sample-projects/chat/server/resolvers.js
+++ 0_sample-projects/chat/server/resolvers.js
@@ -1,24 +1,40 @@
-const db = require('./db');
+const { PubSub } = require("graphql-subscriptions");
+const db = require("./db");
+
+const pubSub = new PubSub();
+const MESSAGE_ADDED = "MESSAGE_ADDED";
 
 function requireAuth(userId) {
   if (!userId) {
-    throw new Error('Unauthorized');
+    throw new Error("Unauthorized");
   }
 }
 
 const Query = {
-  messages: (_root, _args, {userId}) => {
+  messages: (_root, _args, { userId }) => {
     requireAuth(userId);
     return db.messages.list();
-  }
-}
+  },
+};
 
 const Mutation = {
-  addMessage: (_root, {input}, {userId}) => {
+  addMessage: (_root, { input }, { userId }) => {
     requireAuth(userId);
-    const messageId = db.messages.create({from: userId, text: input.text});
-    return db.messages.get(messageId);
-  }
-}
+    const messageId = db.messages.create({ from: userId, text: input.text });
 
-module.exports = { Query, Mutation };
+    // notify clients that have subscribed that a new entry was created
+    const message = db.messages.get(messageId);
+    pubSub.publish(MESSAGE_ADDED, { messageAdded: message });
+
+    // save data to the database
+    return message;
+  },
+};
+
+const Subscription = {
+  messageAdded: {
+    subscribe: () => pubSub.asyncIterator(MESSAGE_ADDED),
+  },
+};
+
+module.exports = { Query, Mutation, Subscription };
